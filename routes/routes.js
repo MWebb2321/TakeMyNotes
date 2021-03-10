980d29f7eeba2753afa055390b080577f86b4371
Matthew Webb <mwebb2321@gmail.com>
2021-03-09 18:15:47 -0800

    making small corrections

--- routes/routes.js
+++ routes/routes.js
@@ -1,45 +1,16 @@
-const fs = reuire('fs');
 const path = require('path');
 
-module.exports = app => {
-    fs.readFile("db/db.json", "utf8", (err, data) => {
-    if (err) throw err;
-    var notes = json.parse(data);
+module.exports = (app) => {
+    // => HTML GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases the user is shown an HTML page of content
 
-    app.get("/api/notes", function(req, res) {
-        res.json(notes);
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
 
-    app.post("/api/notes", function(req, res) {
-        let newNote = req.body;
-        notes.push(newNote);
-        updateDb();
-        return console.log("Added new note: "+newNote.title);
+    // If no matching route is found default to home
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../public/index.html'));
     });
-
-    app.get("/api/notes/:id", function(req,res) {
-        res.json(notes[req.params.id]);
-    });
-
-    app.delete("/api/notes/:id", function(req, res) {
-        notes.splice(req.params.id, 1);
-        updateDb();
-        console.log("Deleted note with id "+req.params.id);
-    });
-
-    app.get('/notes', function(req,res) {
-        res.sendFile(path.join(__dirname, "../public/notes.html"));
-    });
-
-    app.get('*', function(req,res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"));
-    });
-
-    function updateDb() {
-        fs.writeFile("db/db.json",JSON.stringify(notes,'\t'),err => {
-            if (err) throw err;
-            return true;
-        });
-    }
-});
-}
+};
