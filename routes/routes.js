6910785779191f78266dcd50be46b7e31d3dc5b1
Matthew Webb <mwebb2321@gmail.com>
2021-03-09 16:29:08 -0800

    working on routes file

--- routes/routes.js
+++ routes/routes.js
@@ -1,2 +1,14 @@
 const fs = reuire('fs');
 const path = require('path');
+
+module.exports = app => {
+    fs.readFile("db/db.json", "utf8", (err, data) =>
+    if (err) throw err;
+    var notes = json.parse(data);
+
+    app.get("/api/notes", function(req, res) {
+        res.json(notes);
+    });
+
+    
+}
