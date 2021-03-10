99e055c38a4bc050744e9a93f1e6e8aa6376ab1b
Matthew Webb <mwebb2321@gmail.com>
2021-03-09 17:04:33 -0800

    fixed small error on routes file

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const fs = reuire('fs');
 const path = require('path');
 
 module.exports = app => {
-    fs.readFile("db/db.json", "utf8", (err, data) =>
+    fs.readFile("db/db.json", "utf8", (err, data) => {
     if (err) throw err;
     var notes = json.parse(data);
 
