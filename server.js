84239b85a2f2492814698877537cb2a727f4cec2
Matthew Webb <mwebb2321@gmail.com>
2021-03-09 17:25:38 -0800

    fixed small error

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const fs = require("fs");
-const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -8,8 +7,9 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use(express.static(__dirname));
 
-require("./routes/routes")(app);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+app.listen(PORT, () => {
+    console.log("App listening on PORT: ${PORT}");
 });
