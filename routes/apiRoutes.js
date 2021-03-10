47515e5bdc4287dbd6ea004749bf60cd686cb206
Matthew Webb <mwebb2321@gmail.com>
2021-03-09 18:08:10 -0800

    worked on file

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,19 @@
 const fs = require("fs");
 const path = require("path");
 
-module.exports = (app)
+module.exports = (app) => {
+    app.get("/api/notes", (req, res) => {
+        let notes = fs.readFileSync(path.join(__dirname, "../db/db.json"), 'utf8');
+        notes = JSON.parse(notes);
+        console.log(notes);
+        res.json(notes);
+    })
+
+    app.post("/api/notes", (req,res) => {
+        let notes = fs.readFileSync(path.join(_dirname, "../db/db.json"), "utf8");
+        notes = JSON.parse(notes);
+        notes.push(req.body);
+        fs.writeFileSync(path.join(_dirname, "../db/db.json"), JSON.stringify(notes), "utf8");
+        res.json(notes);
+    });
+}
